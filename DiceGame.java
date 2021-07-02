a423b566807e442cbd58c8de4f407cc484e67ec8
ked <kaede12kazu22@gmail.com>
2021-07-02 14:18:03 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -3,9 +3,16 @@ import java.util.*;
 class DiceGame{
     public static void main(String args[]){
         Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+
         int a = rand.nextInt(6) + 1;
         int b = rand.nextInt(6) + 1;
         int sum = a + b;
+        String name;
+
+        System.out.print("What is your name?\n> ");
+        name = scan.nextLine();
+        System.out.println("Hello, " + name + "!");
         System.out.println("Rolling the dice...");
         System.out.println("Die 1: " + a);
         System.out.println("Die 2: " + b);
